73b5e0e20416358481fce68a0ac0008434a9a0ea
Felipe Schreiber Fernandes <schreiber.felipe@gmail.com>
2023-01-31 21:41:49 -0300

    "Changed pyspark file source"

--- etl/01_parquet_creation.py
+++ etl/01_parquet_creation.py
@@ -14,7 +14,7 @@ spark = (
 # leitura inicial dos dados em CSV
 rais = (
     spark.read
-    .csv("s3://datalake-felipeschreiber-desafio/raw-data/rais/", inferSchema=True, header=True, sep=';', encoding="latin1")
+    .csv("s3://datalake-felipeschreiber-desafio/rais/raw-data/", inferSchema=True, header=True, sep=';', encoding="latin1")
 )
 
 
@@ -118,7 +118,7 @@ rais = (
     .write.mode('overwrite')
     .partitionBy('ano', 'uf')
     .format('parquet')
-    .save('s3://datalake-felipeschreiber-desafio/staging-zone/rais/')
+    .save('s3://datalake-felipeschreiber-desafio/rais/staging-zone')
 )
 
 
