d0b4f8adf93f3fe2125da68eb15e4bf52a88e56f
Felipe Schreiber Fernandes <schreiber.felipe@gmail.com>
2023-01-31 21:27:00 -0300

    "Changed bucket name"

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,23 +1,5 @@
 resource "aws_s3_bucket" "dl" {
-  bucket = "datalake-felipeschreiber-desafio"
-  acl    = "private"
-
-  tags = {
-    IES   = "IGTI",
-    CURSO = "EDC"
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket" "stream" {
-  bucket = "schreiber-streaming-bucket"
+  bucket = "bucket4terraform"
   acl    = "private"
 
   tags = {
